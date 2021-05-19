209ad3c401863d6cd15bcd8cf4eb2d1182c9f86a
K Anarde <57640439+anardek@users.noreply.github.com>
2021-05-19 15:32:23 -0400

    slimmed comments, added example inputs

--- R/Extras/scrape_webcams.R
+++ R/Extras/scrape_webcams.R
@@ -1,51 +1,45 @@
-library(dplyr)
-library(lubridate)
-library(readr)
-library(httr)
 library(magick)
 
-# set the working directory
-
-# retrieve the image. We will need to know the time it was downloaded, so maybe this can be achieved by updating a reactive values list, where each site has it's own "time" value
-
-# GetTrafficCam('Mirlo')
-get_traffic_cam <- function(camera_name) {
+# get_traffic_cam('Mirlo')
+# get_traffic_cam('NorthDock')
+# get_traffic_cam('SouthDock')
+# get_traffic_cam('SouthOcracoke')
+get_traffic_cam <- function(camera_name){
 
     # from   https://www.drivenc.gov/
-    Mirlo <- 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_MirloBeach.jpg'
+    if (camera_name == 'Mirlo'){
+        URL <- 'https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_MirloBeach.jpg'
+    }
+    if (camera_name == 'NorthDock'){
+        URL <- 'https://tims.ncdot.gov/TIMS/Cameras/viewimage.ashx?id=Hatteras_Inlet_North_Dock.jpg'
+    }
+    if (camera_name == 'SouthDock'){
+        URL <- 'https://tims.ncdot.gov/TIMS/Cameras/viewimage.ashx?id=Hatteras_Inlet_South_Dock.jpg'
+    }
+    if (camera_name == 'SouthOcracoke'){
+        URL <- 'https://tims.ncdot.gov/tims/cameras/viewimage.ashx?id=Ocracoke_South.jpg'
+    }
+
     # Ocracoke <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_OcracokeNorth.jpg"
     # Hatteras <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_NorthHatterasVillage.jpg"
     # Buxton <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_Buxton.jpg"
     # NewInlet <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_NewInlet.jpg"
     # Canal <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=NC12_CanalZone.jpg"
-    NorthDock <- "https://tims.ncdot.gov/TIMS/Cameras/viewimage.ashx?id=Hatteras_Inlet_North_Dock.jpg"
-    SouthDock <- "https://tims.ncdot.gov/TIMS/Cameras/viewimage.ashx?id=Hatteras_Inlet_South_Dock.jpg"
-    SouthOcracoke <- "https://tims.ncdot.gov/tims/cameras/viewimage.ashx?id=Ocracoke_South.jpg"
-    RBNurl <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=RodantheBridgeNorth.jpg"
+    # RBNurl <- "https://tims.ncdot.gov/TIMS/cameras/viewimage.ashx?id=RodantheBridgeNorth.jpg"
 
-    tmpfile <- tempfile(fileext = ".jpg")  # this file will need to be saved
+    tmpfile <- tempfile(fileext = ".jpg")  # this file will need to be sent to GoogleDrive eventually: do in Shiny?
     #tmpfile2 <- tempfile(fileext = ".jpg")
 
     # retrieve the image
     pic <- magick::image_read(URL)
-    time <-  Sys.time() %>% lubridate::with_tz("UTC")
+    #time <-  Sys.time() %>% lubridate::with_tz("UTC")
 
     # write the image to temporary file. This will be handy for Shiny where renderImage requires an "outfile".
-    pic %>% magick::image_write(tmpfile)
-
-    # Scale image to correct dimensions & write to other temp file. Could also use built-in keras functions
-    #pic %>%
-    #    magick::image_scale(geometry = geometry_size_pixels(width=224,height=224,preserve_aspect = F)) %>%
-    #    magick::image_write(tmpfile2)
+    magick::image_write(pic, path = tmpfile, format = "jpg")
+    #magick::image_write(pic, path = 'test.jpg', format = "jpg")
 
     return(tmpfile)
 }
-
-    ##determine image name
-    #ImName = camera + '/' + str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")) + '-' + camera + '.jpg'
-
-    #save image
-    #os.rename('dummy.jpg', ImName)
 
 
 
