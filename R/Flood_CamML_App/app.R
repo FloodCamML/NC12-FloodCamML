a4e04faea18706f1726fa8992c64a9ef62c67330
Adam <adamkemberling@gmail.com>
2021-05-19 13:42:57 -0400

    core app structure

--- R/Flood_CamML_App/app.R
+++ R/Flood_CamML_App/app.R
@@ -1,49 +1,247 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
+
+
+# Packages  ---------------------------------------------------------------------
 library(shiny)
+library(shinymaterial)
+library(datasets)
+library(keras)
+library(magick)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
+
+####_____________________________####
+####  Load Workspace  ####
+
+
+## 1. Load Models ---------------------------------------------------------------------
+
+
+
+
+
+
+
+## 2. Load NCDOT Images ---------------------------------------------------------------------
+
+
+
+
+
+## 3. Classify Images ---------------------------------------------------------------------
+
+
+
+
+####_____________________________####
+# UI ---------------------------------------------------------------------
+ui <- material_page(
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+    title = "NCDOT FloodCamML", 
+    include_nav_bar = FALSE,
+    primary_theme_color = "#00695c", 
+    secondary_theme_color = "#00796b",
+    
+    
+    
+    ## Header Image ----------------------------------
+    # Image in folder 'www' at same level as app.R
+    material_parallax(
+        # image source: https://obxstuff.com/pages/things-to-do
+        image_source = "outer_banks_aerial.jpeg"
+    ),
+    
+    
+    ## App Description ----------------------------------
+    material_row(
+        material_column(
+            width = 12,
+            material_card(
+                title = "About the App",
+                h2("Subheader"),
+                p("Paragraph explanation.")
+            )
         )
+    ),
+    
+    
+    ####  Define tabs  ####
+    material_tabs(
+        tabs = c(
+            "Sunny Day Flooding"      = "first_tab",
+            "Model 1 Classifications" = "second_tab",
+            "Model 2 Classifications" = "third_tab"
+        )
+    ),
+    
+    
+    #### Tab 1 Content - Background on Flooding  ####
+    material_tab_content(
+        tab_id = "first_tab",
+        material_card(
+            title = "About High Tide Flooding",
+            plotOutput("world_map")
+        ),
+    ),
+    
+    
+    
+    
+    #### Tab 2 Content - Model 1 Results  ####
+    material_tab_content(
+        tab_id = "second_tab",
+        ###_ Row 1 ----------------------------------
+        material_row(
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Okracoke",
+                    plotOutput("faithfulPlot")
+                )
+            ),
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Nags Head",
+                    plotOutput("irisPlot")
+                )
+            )
+        ),
+        
+        ###_ Row 2 ----------------------------------
+        material_row(
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Duck",
+                    plotOutput("mtcarsPlot")
+                )
+            ),
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Carolla",
+                    plotOutput("airmilesPlot")
+                )
+            )
+        ),
+        
+        
+        ###_ Row 3 ----------------------------------
+        material_row(
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Kittyhawk",
+                    plotOutput("mtcarsPlot")
+                )
+            ),
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Avon",
+                    plotOutput("airmilesPlot")
+                )
+            )
+        )
+    ),
+    
+    
+    
+    
+    #### Tab 3 Content - Model 1 Results  ####
+    material_tab_content(
+        tab_id = "third_tab",
+        ###_ Row 1 ----------------------------------
+        material_row(
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Okracoke",
+                    plotOutput("faithfulPlot")
+                )
+            ),
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Nags Head",
+                    plotOutput("irisPlot")
+                )
+            )
+        ),
+        
+        ###_ Row 2 ----------------------------------
+        material_row(
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Duck",
+                    plotOutput("mtcarsPlot")
+                )
+            ),
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Carolla",
+                    plotOutput("airmilesPlot")
+                )
+            )
+        ),
+        
+        
+        ###_ Row 3 ----------------------------------
+        material_row(
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Kittyhawk",
+                    plotOutput("mtcarsPlot")
+                )
+            ),
+            material_column(
+                width = 6,
+                material_card(
+                    title = "Avon",
+                    plotOutput("airmilesPlot")
+                )
+            )
+        )
+    ),
+   
+    
+    
+    
+    ## Tail Image ----------------------------------
+    # Image in folder 'www' at same level as app.R
+    material_parallax(
+        # image source: https://obxstuff.com/pages/things-to-do
+        image_source = "outer_banks_aerial.jpeg"
     )
 )
 
-# Define server logic required to draw a histogram
+
+
+
+
+
+
+####_____________________________####
+# SERVER ---------------------------------------------------------------------
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$mtcarsPlot <- renderPlot({
+        plot(mtcars)
+    })
+    output$irisPlot <- renderPlot({
+        plot(iris)
+    })
+    
+    output$faithfulPlot <- renderPlot({
+        plot(faithful)
+    })
+    output$airmilesPlot <- renderPlot({
+        plot(airmiles)
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
